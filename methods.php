75a09d8f0a060402a9349f96b70eeb6defb0be58
umargarga <udygarga@gmail.com>
2017-01-24 17:36:03 +0000

    Create a restaurant class

--- methods.php
+++ methods.php
@@ -1,1 +1,55 @@
+class restaurant {
+    private String id;
+    private String title;
+    private String address;
+    private String description;
+    private String type;
 
+    public restaurant(String id, String title, String address, String description, String type) {
+        this.id = id;
+        this.title = title;
+        this.address = address;
+        this.description = description;
+        this.type = type;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}

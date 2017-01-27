9070d6f53fc7bc99949bfdc1e6c2af5d9a04f7bc
Tayseer83 <tayseer_ama@yahoo.com>
2017-01-27 13:55:38 +0000

    Update methods.php

--- methods.php
+++ methods.php
@@ -1,51 +1,107 @@
+package com.tayseerproject.eatandenjoy;
 
-int selectedId = radioGroup.getCheckedRadioButtonId();
-                // find the radiobutton by returned id
-                radioButton = (RadioButton) findViewById(selectedId);
-                brandName = (EditText) findViewById(R.id.brand);
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
-                brand_name = brandName.getText().toString();
-                brand_category = radioButton.getText().toString();
+import org.json.JSONObject;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class RestaurantRegisterActivity extends AppCompatActivity {
+    EditText title,description,address;
+    RadioGroup radioGroup;
+    RadioButton radioButton;
+    Button register;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant_register);
+
+        title = (EditText) findViewById(R.id.titleText);
+        description = (EditText) findViewById(R.id.descriptionText);
+        address = (EditText) findViewById(R.id.addressText);
+
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+
+        register = (Button) findViewById(R.id.registerButton);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int radioSelected = radioGroup.getCheckedRadioButtonId();
+                radioButton = (RadioButton) findViewById(radioSelected);
 
 
-public class HttpTask extends AsyncTask<String, String, String> {
+                String titleValue, descriptionValue, addressValue, typeValue;
+                titleValue = title.getText().toString();
+                descriptionValue = description.getText().toString();
+                addressValue = address.getText().toString();
+                typeValue = radioButton.getText().toString();
+
+
+                new HttpTask().execute(titleValue,descriptionValue,addressValue,typeValue);
+
+
+            }
+        });
+    }
+
+
+    public class HttpTask extends AsyncTask<String, String, String> {
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
-            Toast.makeText(getBaseContext(), "Company Registration Successful", Toast.LENGTH_LONG).show();
-            //Intent i = new Intent(RegisterActivthis, LoginActivity.class);
-            //startActivity(i);
+            Toast.makeText(getBaseContext(), "Registration Successful", Toast.LENGTH_LONG).show();
+            Intent i = new Intent(RestaurantRegisterActivity.this, MainActivity.class);
+            startActivity(i);
         }
 
 
         @Override
         protected String doInBackground(String... params) {
             URL url;
-            HttpURLConnection connection;
+            HttpURLConnection con;
             try {
-                url = new URL(params[0]);
-                connection = (HttpURLConnection) url.openConnection();
+                url = new URL("http://10.0.2.2:8888/v1/restaurant");
+                con = (HttpURLConnection) url.openConnection();
+                String title,description,address,type;
+                title = params[0];
+                description = params[1];
+                address = params[2];
+                type = params[3];
 
-                if (params[1].equals("POST")) {
-                    connection.setRequestMethod("POST");
-                    JSONObject data = new JSONObject();
-                    data.put("Users_ID", "1");
-                   
 
-                    connection.setDoInput(true);
-                    connection.setDoOutput(true);
-                    connection.setRequestProperty("Content-Type", "application/json");
+                con.setRequestMethod("POST");
+                JSONObject data = new JSONObject();
+                data.put("title", title);
+                data.put("address", address);
+                data.put("description", description);
+                data.put("type", type);
+                data.put("rating","0.0");
 
-                    if (data != null) {
-                        DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream());
-                        outputStream.writeBytes(data.toString());
-                        outputStream.flush();
-                        outputStream.close();
-                    }
-                    int responseCode = connection.getResponseCode();
+                con.setDoInput(true);
+                con.setDoOutput(true);
+                con.setRequestProperty("Content-Type", "application/json");
 
-                }else{
-                    finish();
+                if (data != null) {
+                    DataOutputStream outputStream = new DataOutputStream(con.getOutputStream());
+                    outputStream.writeBytes(data.toString());
+                    outputStream.flush();
+                    outputStream.close();
                 }
 
             } catch (MalformedURLException e) {
@@ -58,3 +114,4 @@ public class HttpTask extends AsyncTask<String, String, String> {
             return null;
         }
     }
+}

065824d9f5f4a5908213759f6a9a1d211507af7e
umargarga <udygarga@gmail.com>
2017-01-29 14:02:44 +0000

    Update methods.php

--- methods.php
+++ methods.php
@@ -1,117 +1,40 @@
-package com.tayseerproject.eatandenjoy;
 
-import android.content.Intent;
-import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.Toast;
-
-import org.json.JSONObject;
-
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-
-public class RestaurantRegisterActivity extends AppCompatActivity {
-    EditText title,description,address;
-    RadioGroup radioGroup;
-    RadioButton radioButton;
-    Button register;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_restaurant_register);
-
-        title = (EditText) findViewById(R.id.titleText);
-        description = (EditText) findViewById(R.id.descriptionText);
-        address = (EditText) findViewById(R.id.addressText);
-
-        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
-
-        register = (Button) findViewById(R.id.registerButton);
-
-        register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                int radioSelected = radioGroup.getCheckedRadioButtonId();
-                radioButton = (RadioButton) findViewById(radioSelected);
-
-
-                String titleValue, descriptionValue, addressValue, typeValue;
-                titleValue = title.getText().toString();
-                descriptionValue = description.getText().toString();
-                addressValue = address.getText().toString();
-                typeValue = radioButton.getText().toString();
-
-
-                new HttpTask().execute(titleValue,descriptionValue,addressValue,typeValue);
-
-
-            }
-        });
-    }
-
-
-    public class HttpTask extends AsyncTask<String, String, String> {
-        @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
-            Toast.makeText(getBaseContext(), "Registration Successful", Toast.LENGTH_LONG).show();
-            Intent i = new Intent(RestaurantRegisterActivity.this, MainActivity.class);
-            startActivity(i);
-        }
-
-
-        @Override
-        protected String doInBackground(String... params) {
-            URL url;
-            HttpURLConnection con;
+URL url;
+            HttpURLConnection connection;
             try {
-                url = new URL("http://10.0.2.2:8888/v1/restaurant");
-                con = (HttpURLConnection) url.openConnection();
-                String title,description,address,type;
-                title = params[0];
-                description = params[1];
-                address = params[2];
-                type = params[3];
+                url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
 
+                if (params[1].equals("POST")) {
+                    connection.setRequestMethod("POST");
+                    JSONObject data = new JSONObject();
+                    data.put("Users_ID", "1");
+                    data.put("BrandName", params[2]);
+                    data.put("Category", params[3]);
+                    data.put("Description", params[4]);
+                    data.put("RatingCount", "0");
+                    data.put("AverageRating", "0.0");
+                    data.put("Address", params[5]);
+                    data.put("City", params[6]);
+                    data.put("PostCode", params[7]);
 
-                con.setRequestMethod("POST");
-                JSONObject data = new JSONObject();
-                data.put("title", title);
-                data.put("address", address);
-                data.put("description", description);
-                data.put("type", type);
-                data.put("rating","0.0");
+                    connection.setDoInput(true);
+                    connection.setDoOutput(true);
+                    connection.setRequestProperty("Content-Type", "application/json");
 
-                con.setDoInput(true);
-                con.setDoOutput(true);
-                con.setRequestProperty("Content-Type", "application/json");
+                    if (data != null) {
+                        DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream());
+                        outputStream.writeBytes(data.toString());
+                        outputStream.flush();
+                        outputStream.close();
+                    }
+                    int responseCode = connection.getResponseCode();
 
-                if (data != null) {
-                    DataOutputStream outputStream = new DataOutputStream(con.getOutputStream());
-                    outputStream.writeBytes(data.toString());
-                    outputStream.flush();
-                    outputStream.close();
+                }else{
+                    finish();
                 }
 
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
             } catch(Exception e){
                 e.printStackTrace();
             }
             return null;
-        }
-    }
-}

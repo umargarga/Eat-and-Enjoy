e8c68ca932c38c0b4fb57de7bc67f176f844302e
umargarga <udygarga@gmail.com>
2017-01-29 23:53:35 +0000

    Update methods.php

--- methods.php
+++ methods.php
@@ -1,7 +1,37 @@
-$api->get('/companies/{id}', function ($id) use ($app) {
-    $sql = "SELECT * FROM company WHERE CompanyID = $id";
-    $company = $app['db']->fetchAll($sql);
-    return $app->json(array('company' => $company));
-    //$value = $app->json($users);
-    //return $value;
-});
+
+
+<LinearLayout
+            android:layout_width="wrap_content"
+            android:layout_height="wrap_content"
+            android:paddingLeft="16sp"
+            android:orientation="vertical">
+            <TextView
+                android:layout_width="wrap_content"
+                android:layout_height="wrap_content"
+                android:id="@+id/title"
+                android:textStyle="bold"/>
+            <TextView
+                android:layout_width="wrap_content"
+                android:layout_height="wrap_content"
+                android:id="@+id/type"/>
+    
+            <TextView
+                android:layout_width="wrap_content"
+                android:layout_height="wrap_content"
+                android:id="@+id/address"
+                android:textColor="#000"/>
+
+            <TextView
+                android:layout_width="wrap_content"
+                android:layout_height="wrap_content"
+                android:id="@+id/description"/>
+    
+            <RatingBar
+                android:layout_width="wrap_content"
+                android:layout_height="wrap_content"
+                android:id="@+id/ratingBar"
+                style="?android:attr/ratingBarStyleSmall"
+                android:numStars="5"
+                android:stepSize="0.1"
+                android:isIndicator="true"/>
+        </LinearLayout>

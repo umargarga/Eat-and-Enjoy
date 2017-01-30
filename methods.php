a720400faac030dfeafcfb245d80c3224b90c965
umargarga <udygarga@gmail.com>
2017-01-30 00:30:32 +0000

    Update methods.php

--- methods.php
+++ methods.php
@@ -1,37 +1,83 @@
 
 
-<LinearLayout
-            android:layout_width="wrap_content"
-            android:layout_height="wrap_content"
-            android:paddingLeft="16sp"
-            android:orientation="vertical">
-            <TextView
-                android:layout_width="wrap_content"
-                android:layout_height="wrap_content"
-                android:id="@+id/title"
-                android:textStyle="bold"/>
-            <TextView
-                android:layout_width="wrap_content"
-                android:layout_height="wrap_content"
-                android:id="@+id/type"/>
-    
-            <TextView
-                android:layout_width="wrap_content"
-                android:layout_height="wrap_content"
-                android:id="@+id/address"
-                android:textColor="#000"/>
+class HttpTask extends AsyncTask<String, String, String>{
+        @Override
+        protected void onPreExecute() {
+            ProgressDialog progressDialog = ProgressDialog.show(IndividualRestaurantActivity.this, "Loading", "Please wait...");
+            progressDialog.setCancelable(false);
+        }
 
-            <TextView
-                android:layout_width="wrap_content"
-                android:layout_height="wrap_content"
-                android:id="@+id/description"/>
-    
-            <RatingBar
-                android:layout_width="wrap_content"
-                android:layout_height="wrap_content"
-                android:id="@+id/ratingBar"
-                style="?android:attr/ratingBarStyleSmall"
-                android:numStars="5"
-                android:stepSize="0.1"
-                android:isIndicator="true"/>
-        </LinearLayout>
+        @Override
+        protected String doInBackground(String... params) {
+            String result;
+            URL url;
+            HttpURLConnection connection = null;
+            try {
+                url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+
+                result = "";
+                    connection.setRequestMethod("GET");
+
+                    //int responseCode = connection.getResponseCode();
+                    InputStream is = connection.getInputStream();
+                    if (is != null) {
+                        BufferedReader bufferReader =
+                                new BufferedReader(new InputStreamReader(is));
+                        String line;
+                        while ((line = bufferReader.readLine()) != null)
+                            result += line;
+                        is.close();
+                        return result;
+                    }
+
+            }
+            catch (Exception e) {
+                result=""+e.getMessage();
+            }
+            finally {
+                if(connection!=null)
+                    connection.disconnect();
+            }
+
+
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            if(progressDialog.isShowing())
+                progressDialog.dismiss();
+
+            super.onPostExecute(s);
+            //tv1.setText(R.string.result + s);
+            JSONObject object;
+            String object_title;
+            String object_type;
+            String object_address;
+            String object_description;
+            String object_rating;
+
+            try {
+                object = new JSONObject(s);
+                JSONObject object2 = object.getJSONArray("restaurant").getJSONObject(0);
+
+                object_title = object2.getString("title");
+                object_type = object2.getString("type");
+                object_address = object2.getString("address");
+                object_description = object2.getString("description");
+                object_rating = object2.get("rating");
+
+
+                title.setText(object_title);
+                type.setText(object_type);
+                address.setText(object_address);
+                description.setText(object_description);
+                rating.setRating(Float.parseFloat(object_rating));
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
